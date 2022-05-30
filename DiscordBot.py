1bc8e8232650dc1b673d3e60c03991a731bf0732
devotee <info@terraria.by>
2022-05-30 11:08:38 +0300

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -13,7 +13,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
 
-# автореакт ideas
+# автореакт
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
