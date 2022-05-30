20c4780fb426201e53d48a32381e17e12e1ecd05
devotee <info@terraria.by>
2022-05-30 15:37:51 +0300

    Add files via upload

--- DiscordBot.py
+++ DiscordBot.py
@@ -34,12 +34,12 @@ async def help_command(ctx: commands.context.Context):
     help_embed.set_author(name='Help')
     help_embed.add_field(name='clear', value='clear <кол-во> \nОчищает чат.', inline=False)
     help_embed.add_field(name='mute',
-                         value='mute <никнейм> <время(s/m/h)> <причина>'
+                         value='mute <никнейм> <время(m/h)> <причина>'
                                '\nЗапрещает участнику писать в чат.',
                          inline=False)
     help_embed.add_field(name='unmute', value='unmute <никнейм> \nСнимает с участника мут.', inline=False)
-    help_embed.set_footer(text='Версия 1.1.3')
-    await ctx.send(embed=help_embed)
+    help_embed.set_footer(text='Версия 1.1.4')
+    await ctx.author.send(embed=help_embed)
 
 
 # очистка чата
@@ -70,12 +70,19 @@ async def clear_command(ctx: commands.context.Context, amount: int):
 @bot.command(name='mute')
 @commands.has_role(888860282976342096)
 async def mute_command(ctx: commands.context, member: discord.Member, mute_time, reason):
+    end_time = mute_time[-1:]
+    time = int(mute_time[:-1])
     mute_role = discord.utils.get(ctx.guild.roles, id=705885683566510140)
     if not ctx.channel.id == 393868048475488256 or ctx.channel.id == 844150788053532712:
         await ctx.message.delete()
         mute_embed = discord.Embed(
             title='_<:wNope:863417394920030208>_ **Эту команду нельзя использовать здесь!**',
             color=0xD11818)
+        await ctx.send(embed=mute_embed, delete_after=5)
+    elif not end_time == 'm' or end_time == 'h':
+        await ctx.message.delete()
+        mute_embed = discord.Embed(title='_<:wNope:863417394920030208>_ **Неверный формат времени!**',
+                                   color=0xD11818)
         await ctx.send(embed=mute_embed, delete_after=5)
     elif member.id == ctx.author.id:
         mute_embed = discord.Embed(title='_<:wNope:863417394920030208>_ **Нельзя выдать мут самому себе!**',
@@ -87,8 +94,6 @@ async def mute_command(ctx: commands.context, member: discord.Member, mute_time,
             title='_<:wNope:863417394920030208>_ **Нельзя выдать мут участнику с такой же ролью, как у вас или выше!**',
             color=0xD11818)
         await ctx.send(embed=mute_embed, delete_after=5)
-    end_time = mute_time[-1:]
-    time = int(mute_time[:-1])
     if time <= 0:
         await ctx.message.delete()
         mute_embed = discord.Embed(
@@ -101,18 +106,28 @@ async def mute_command(ctx: commands.context, member: discord.Member, mute_time,
             title=f'_<:wYep:863417394622103563>_ **{member} получает мут по причине: {reason}!**',
             color=0x20D714)
         await ctx.send(embed=mute_embed)
-        if end_time == 's':
+        if end_time == 'm':
             await member.add_roles(mute_role)
-            await asyncio.sleep(time)
-            await member.remove_roles(mute_role)
-        elif end_time == 'm':
-            await member.add_roles(mute_role)
+            mute_embed = discord.Embed(title=':face_with_symbols_over_mouth:  **Вы получили мут!**', color=0xFF0000)
+            mute_embed.add_field(name='Время', value=f'{time} минут(ы)', inline=False)
+            mute_embed.add_field(name='Причина', value=f'{reason}', inline=False)
+            mute_embed.add_field(name='Исполнитель', value=f'{ctx.author}', inline=False)
+            await member.send(embed=mute_embed)
             await asyncio.sleep(time * 60)
             await member.remove_roles(mute_role)
+            mute_embed = discord.Embed(title='**:timer:  Время мута истекло!**', color=0x20D714)
+            await member.send(embed=mute_embed)
         elif end_time == 'h':
             await member.add_roles(mute_role)
+            mute_embed = discord.Embed(title=':face_with_symbols_over_mouth:  **Вы получили мут!**', color=0xFF0000)
+            mute_embed.add_field(name='Время', value=f'{time} час(ов)', inline=False)
+            mute_embed.add_field(name='Причина', value=f'{reason}', inline=False)
+            mute_embed.add_field(name='Исполнитель', value=f'{ctx.author}', inline=False)
+            await member.send(embed=mute_embed)
             await asyncio.sleep(time * 3600)
             await member.remove_roles(mute_role)
+            mute_embed = discord.Embed(title='**:timer:  Время мута истекло!**', color=0x20D714)
+            await member.send(embed=mute_embed)
 
 
 # анмут
@@ -137,12 +152,15 @@ async def un_mute(ctx: commands.context, member: discord.Member):
         unmute_embed = discord.Embed(title=f'_<:wYep:863417394622103563>_ **С {member} снят мут!**',
                                      color=0x20D714)
         await ctx.send(embed=unmute_embed)
+        unmute_embed = discord.Embed(title=':innocent:  **Вам сняли мут!**', color=0x20D714)
+        unmute_embed.add_field(name='Исполнитель', value=f'{ctx.author}', inline=False)
+        await member.send(embed=unmute_embed)
 
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
-        error_embed = discord.Embed(title=':alarm_clock:  **Пока что нельзя использовать эту команду!**',
+        error_embed = discord.Embed(title=':timer:  **Пока что нельзя использовать эту команду!**',
                                     color=0xFFF404)
         await ctx.message.delete()
         await ctx.send(embed=error_embed, delete_after=5)
@@ -170,4 +188,4 @@ if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.starts
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 # token
-bot.run('...')
+bot.run('OTc3MjMyNzc0MDU2NTI5OTky.G48VWu.18UAuc6LTZ3VBDGBYLBulnv78ZaKFgwtOlIpDQ')
